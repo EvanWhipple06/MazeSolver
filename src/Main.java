3e9c15b2df4ccd2bfbc43accd9cadb596664c47d
Evan Whipple <evanwhipple06@gmail.com>
2024-11-27 20:04:20 -0500

    created simple maze solver for 1 path mazes

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,36 @@
 public class Main {
     public static void main(String args[]) {
-        char wall = '#';
-        char empty = ' ';
-        char path = '•';
+        String wall = "#";                  //just setting some characters that will never change, i should probably make them constants but I dont know if ill use them here yet
+        String empty = " ";
+        String path = "•";
 
-        String maze = "##########" +
-                "\n###S    ##" +
-                "\n####### ##" +
-                "\n##E     ##" +
-                "\n##########";
+        String maze = "##########" +       //the maze to be solved, exchange with a valid maze as you desire ONCE ITS FINISHED SO NOT YET LOL
+                    "\n###S    ##" +
+                    "\n####### ##" +
+                    "\n##E     ##" +
+                    "\n##########";
+//        String maze = "" +                //a second maze for testing
+//                  "#########################" +
+//                "\n#S        ###           #" +
+//                "\n######### #   ######### #" +
+//                "\n###       # ##E       # #" +
+//                "\n### ####### ######### # #" +
+//                "\n### #######           # #" +
+//                "\n### ################### #" +
+//                "\n###                     #" +
+//                "\n#########################";
 
-        Main main = new Main();
+        Main main = new Main();             //create main object for future referencing, is this bad code?
 
-        System.out.println(maze);
-        System.out.println("Columns: " + main.getColumns(maze) + "\nRows: " + main.getRows(maze));
+//        System.out.println(maze);           //FOR DEBUGGING: print original maze and the number of rows and columns
+//        System.out.println("Columns: " + main.getColumns(maze) + "\nRows: " + main.getRows(maze));
 
-        String[][] parsedMaze = main.mazeParser(maze);
+        String[][] parsedMaze = main.mazeParser(maze);  //parse the inputted string maze into a 2d array, makes making new mazes easy
 
-        Pathfinding pathfinding = new Pathfinding(main.getColumns(maze), main.getRows(maze), main.getCharX('S', parsedMaze), main.getCharY('S', parsedMaze));
+        Pathfinding pathfinding = new Pathfinding(main.getColumns(maze), main.getRows(maze), main.getCharX("S", parsedMaze), main.getCharY("S", parsedMaze));   //create a pathfinding object
 
-        for(String[] i : parsedMaze) {
-            System.out.println();
-            for(String j : i){
-                System.out.print(j);
-            }
-        }
+        main.solve(parsedMaze, pathfinding);
+        main.printMaze(parsedMaze);
     }
 
     public String[][] mazeParser(String map) {
@@ -39,33 +45,78 @@ public class Main {
         }
 
         return maze;
-    }
+    }                   //parse the string inputted maze into a 2d array so this program actually works
 
     public int getColumns(String map) {
         String[] columns = map.split("\n");
         return columns[0].length();
-    }
+    }                          //determine the number of columns in the inputted maze
 
     public int getRows(String map) {
         String[] rows = map.split("\n");
         return rows.length;
-    }
+    }                             //determine the number of rows in the inputted maze
 
-    public int getCharX(char c, String[][] parsedMaze) {                   //loops through the entire array searching for the first instance of a given character then returns it's x coordinate, otherwise returns bogus number
+    public int getCharX(String c, String[][] parsedMaze) {                   //loops through the entire array searching for the first instance of a given character then returns it's x coordinate, otherwise returns bogus number
         for(int i = 0; i < parsedMaze.length; i++) {
             for(int j = 0; j < parsedMaze[i].length; j++){
-                if(parsedMaze[i][j].equals('c')) return j;
+                if(parsedMaze[i][j].equals(c)) return j;
             }
         }
         return -100000000;
-    }
+    }       //return the 'x' value of a given character, only works properly for unique characters, ie the start and end
 
-    public int getCharY(char c, String[][] parsedMaze) {                   //loops through the entire array searching for the first instance of a given character then returns it's y coordinate, otherwise returns bogus number
+    public int getCharY(String c, String[][] parsedMaze) {                   //loops through the entire array searching for the first instance of a given character then returns it's y coordinate, otherwise returns bogus number
         for(int i = 0; i < parsedMaze.length; i++) {
             for(int j = 0; j < parsedMaze[i].length; j++){
-                if(parsedMaze[i][j].equals('c')) return i;
+                if(parsedMaze[i][j].equals(c)) return i;
             }
         }
         return -100000000;
+    }       //return the 'y' value of a given character, only works properly for unique characters, ie the start and end
+
+    public void printMaze(String[][] map) {                         //prints out the current maze
+        for(String[] i : map) {
+            System.out.println();
+            for (String j : i) {
+                System.out.print(j);
+            }
+        }
+        System.out.println();
+    }
+
+    public void solve(String[][] map, Pathfinding pathfinding) {
+        boolean run = true;                                         //initialize loop variable
+        while(run) {
+            int adjacency = pathfinding.isAdjacent(map, " ");    //set up switch case variable
+            switch(adjacency) {                                     //check what direction the open path is in, move in that direction, mark path as moved on
+                case 1:
+                    pathfinding.moveUp();
+                    pathfinding.write(map);
+                    break;
+                case 2:
+                    pathfinding.moveRight();
+                    pathfinding.write(map);
+                    break;
+                case 3:
+                    pathfinding.moveDown();
+                    pathfinding.write(map);
+                    break;
+                case 4:
+                    pathfinding.moveLeft();
+                    pathfinding.write(map);
+                    break;
+                case 0:
+                    System.out.println("The pathfinder has hit a wall, there is no more possible way to go");
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Default");
+            }
+            if(pathfinding.isAdjacent(map, "E") > 0 ) {          //tests if the path is next to the end of the maze
+                run = false;
+                System.out.println("Maze Complete!");
+            }
+        }
     }
 }
--- src/Pathfinding.java
+++ src/Pathfinding.java
@@ -3,27 +3,52 @@ public class Pathfinding {
     private int y;
     private int columns;
     private int rows;
+    private String wall = "#";
+    private String empty = " ";
+    private String path = "•";
 
-    public Pathfinding(int columns, int rows, int x, int y) {
+    public Pathfinding(int columns, int rows, int x, int y) {   //initializes the class with an x and y based on where S is and the number of columns and rows the maze has
         this.rows = rows;
         this.columns = columns;
         this.x = x;
         this.y = y;
     }
 
-    public void moveLeft() {
+    public void moveLeft() {                                    //move one space to the left, if unable to, do nothing
         if(this.x > 0 && this.x < columns) this.x -= 1;
     }
 
-    public void moveRight() {
+    public void moveRight() {                                   //move one space to the right, if unable to, do nothing
         if(this.x > -1 && this.x < columns - 1) this.x += 1;
     }
 
-    public void moveUp() {
-        if(this.y > 0 && this.y < rows - 1) this.y += 1;
+    public void moveUp() {                                      //move one space up, if unable to, do nothing
+        if(this.y > 0 && this.y < rows - 1) this.y -= 1;
     }
 
-    public void moveDown() {
-        if(this.x > -1 && this.x < rows) this.y -= 1;
+    public void moveDown() {                                    //move one space down, if unable to, do nothing
+        if(this.y > -1 && this.y < rows) this.y += 1;
+    }
+
+    public void write(String[][] map) {                         //change the current space to a dot
+        map[this.y][this.x] = path;
+    }
+
+    public int isAdjacent(String[][] map, String c) {           //checks which side a given character is adjacent to in a provided maze, 1 = top, 2 = right, 3 = bottom, 4 = left, 0 = not adjacent
+        if(map[this.y - 1][this.x].equals(c)) {
+            return 1;
+        } else if(map[this.y][this.x + 1].equals(c)) {
+            return 2;
+        } else if(map[this.y + 1][this.x].equals(c)) {
+            return 3;
+        } else if(map[this.y][this.x - 1].equals(c)) {
+            return 4;
+        } else {
+            return 0;
+        }
+    }
+
+    public String isOnChar(String[][] map) {                    //checks and returns what character the path is currently at
+        return map[this.y][this.x];
     }
 }
